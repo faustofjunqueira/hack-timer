70ef4cf3c6498ce2617f83632fe51fbb846e4436
Fausto Ferreira Junqueira <fausto.junqueira@radixeng.com.br>
2019-07-04 20:31:03 -0300

    +finalizado agenda

--- frontend/src/components/agenda/agenda.jsx
+++ frontend/src/components/agenda/agenda.jsx
@@ -1,56 +1,93 @@
 import React, { useState } from 'react';
 import { getActivities } from './agenda.service';
 
-const Status = {
-  loadign: 'LOADING',
-  now: 'NOW',
-  next: 'NEXT',
-  finish: 'FINISH'
+const getCurrentActivityIndex = (listActivities, time) =>
+  listActivities.indexOf(listActivities.find(a => a.start.getTime() <= time && time <= a.end.getTime()))
+
+const getTitleNowActivity = (listActivities) => {
+  if (listActivities && listActivities.length) {
+    const now = Date.now();
+    if (now < listActivities[0].start.getTime()) {
+      return 'The winter is comming';
+    }
+    if (now > listActivities[listActivities.length - 1].end.getTime()) {
+      return 'Encerrado!'
+    }
+    const activityRunningIndex = getCurrentActivityIndex(listActivities, now);
+    if (activityRunningIndex !== -1) {
+      return listActivities[activityRunningIndex].title;
+    }
+  }
+  return 'Coding';
 }
 
-function getTargetActivity(listActivities) {
+const getTitleNextActivity = (listActivities) => {
   if (listActivities && listActivities.length) {
-    const dateNow = Date.now();
-    const listActivitiesFiltered = listActivities
-      .filter(x => x.end.getTime() > dateNow)
-      .sort((a, b) => a.start.getTime() - b.start.getTime());
-    return listActivitiesFiltered[0];
+    const now = Date.now();
+    if (now < listActivities[0].start.getTime()) {
+      return listActivities[0].title;
+    }
+    if (now > listActivities[listActivities.length - 1].end.getTime()) {
+      return 'Comming Soon';
+    }
+    const activityRunningIndex = getCurrentActivityIndex(listActivities, now) + 1;
+    if (activityRunningIndex) {
+      if (activityRunningIndex === listActivities.length) {
+        return 'Comming Soon';
+      } else {
+        return listActivities[activityRunningIndex].title;
+      }
+    }
+  }
+  return 'Comming Soon';
+}
+
+export class Agenda extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      listActivities: []
+    }
+  }
+
+  componentWillMount() {
+    getActivities().then(listActivities => this.setState({ listActivities }));
+  }
+
+  render() {
+    if (this.state.listActivities && this.state.listActivities.length) {
+      return (
+        <AgendaCollated listActivities={this.state.listActivities} />
+      );
+    } else {
+      return (<div></div>)
+    }
   }
 }
 
-const calculateStatus = (targetActivity) => ({
-  targetActivity,
-  status: targetActivity ? (Date.now() < targetActivity.end.getTime() ? Status.now : Status.next) : Status.finish
-})
+const AgendaCollated = ({ listActivities }) => {
+  const [titles, setTitles] = useState({
+    now: getTitleNowActivity(listActivities),
+    next: getTitleNextActivity(listActivities)
+  });
 
-export const Agenda = (props) => {
-  //const [state, setState] = useState({ status: Status.loadign, targetActivity: null });
-  let listActivities = null;
+  setInterval(() => setTitles({
+    now: getTitleNowActivity(listActivities),
+    next: getTitleNextActivity(listActivities)
+  }), 45000);
 
   return (
     <div className="agenda">
-      <div className="now">
-        <label>Acontecendo Agora</label>
-        <div>Codding...</div>
-      </div>
-      <div className="next">
-        <label>Próximo Evento</label>
-        <div>Pizzaaaaa =D</div>
-      </div>
+      <AgendaCollatedActivity className="now" title="Acontecendo Agora" description={titles.now} />
+      <AgendaCollatedActivity className="next" title="Próximo Evento" description={titles.next} />
     </div>
-  );
-};
+  )
+}
 
-const AgendaActivityNow = ({ activity }) => (
-  <div className="now">
-    <label>Acontecendo Agora</label>
-    <div>Codding...</div>
-  </div>
-);
-
-const AgendaActivityNext = ({ activity }) => (
-  <div className="next">
-    <label>Próximo Evento</label>
-    <div>Pizzaaaaa =D</div>
+(
+  <div className={className}>
+    <label>{title}</label>
+    <div>{description}</div>
   </div>
 );
--- frontend/src/components/board/Board.jsx
+++ frontend/src/components/board/Board.jsx
@@ -8,8 +8,8 @@ export class Board extends React.Component {
   render() {
     return (
       <section>
-        <CountdownTimer onEnd={() => console.log("end")} deadline={(new Date('2019-07-07T12:00:00.000Z')).getTime()} maxTime={36 * 60 * 60 * 1000} />
-        {/* <SocialMediaWall id="medias" /> */}
+        <CountdownTimer onEnd={() => console.log("end")} deadline={(new Date('2019-07-05T16:30:00.000Z')).getTime()} maxTime={36 * 60 * 60 * 1000} />
+        <SocialMediaWall id="medias" />
       </section>
     )
   }
