4b9c1b1a01a96725ae47e0b346222ee3aba586eb
Fausto Ferreira Junqueira <fausto.junqueira@radixeng.com.br>
2019-07-05 01:43:42 -0300

    merge

--- frontend/src/components/app/app.jsx
+++ frontend/src/components/app/app.jsx
@@ -4,14 +4,13 @@ import { Board } from '../board/Board'
 import { SocialMediaWall } from '../social-media/social-media';
 import './app.css'
 
-export class App extends React.Component 
-{
+export class App extends React.Component {
     render() {
         return (
             <Router>
-                <Route path="/" exact component={Board}/>
-                <Route path="/agenda" exact component={Board}/>
-                <Route path="/social" exact component={SocialMediaWall}/>
+                <Route path="/" exact component={Board} />
+                <Route path="/agenda" exact component={Board} />
+                <Route path="/social" exact component={SocialMediaWall} props={{ id: 'medias' }} />
             </Router>
         );
     }
