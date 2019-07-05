d2722df106fa22af012b7a401013833c295a3964
Ubuntu <fausto@HackathonFausto.5mjfsatz1neetigswgaynjlsaa.bx.internal.cloudapp.net>
2019-07-05 07:13:18 +0000

    fetch update

--- frontend/src/utils/fetch.js
+++ frontend/src/utils/fetch.js
@@ -1,6 +1,6 @@
 const hostInfo = {
   defaultBaseUrlDev: 'http://localhost:10000',
-  defaultBaseUrlPrd: 'http://hack.radixeng.com.br:10000',
+  defaultBaseUrlPrd: 'http://hack.radixeng.com.br:8080',
 
   get host() {
     return window.location.hostname === 'localhost' ? this.defaultBaseUrlDev : this.defaultBaseUrlPrd;
